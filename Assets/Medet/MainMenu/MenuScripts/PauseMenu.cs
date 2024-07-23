e7b341169778e999314b34eda19824cdfecfee2b
Medet Çınar <cinarmedet25@gmail.com>
2024-07-23 19:33:49 +0300

    pause menu eklendi

---
+++ Assets/Medet/MainMenu/MenuScripts/PauseMenu.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject optionsScreen,pauseScreen;
+
+    public string mainMenuScene;
+
+
+    private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Cursor.visible=true;
+        Cursor.lockState = CursorLockMode.None;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseunPause();
+        }
+        
+    }
+
+
+    public void PauseunPause()
+    {
+        if(!isPaused)
+        {
+            pauseScreen.SetActive(true);
+            isPaused=true;
+            Time.timeScale=0f; // ekranı donduruyor.
+
+        }
+        else
+        {
+            pauseScreen.SetActive(false);
+            isPaused=false;
+            Time.timeScale = 1f; //ekranı normal hıza döndürüyor
+        }
+
+
+
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+
+    }
+    public void closeOptions()
+    {
+        optionsScreen.SetActive(false);
+
+    }
+    public void QuittoMain()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+
+        Time.timeScale=1f;
+
+    }
+}
